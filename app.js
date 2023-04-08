142227b964e49e9874f9a9a1fd36c8b01f8352de
GOWTHAM BAALAJI K <103375595+gowthamgowthi4@users.noreply.github.com>
2023-04-08 17:09:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,54 @@ app.use(express.json());
 app.use("/", router);
 app.use("/user", userRouter);
 
+const api1 = (req, res) => {
+    let q = "SELECT email FROM userinfo where email = $email"
+  
+    db.query(q, (err, data) => {
+      if (data) return res.status(200).json("User already exists");
+      //res.send('Parkinginfo table created!');
+      return res.status(500).json();
+    });
+};
+
+const api2 = (req, res) => {
+    let q = "INSERT INTO userinfo VALUES($numberplate, $email, $username, $phonenumber, 600)"
+  
+    db.query(q, (err, data) => {
+      if (data) return res.status(500).json();
+      //res.send('Parkinginfo table created!');
+      return res.status(200).json("Error!");
+    });
+};
+
+const api3 = (req, res) => {
+    let q1 = "INSERT INTO occupiedslot(slotname, floor, numberplate, email, username, phonenumber) SELECT freeslot.slotname, freeslot.floor, userinfo.numberplate, userinfo.email, userinfo.username, userinfo.phonenumber FROM freeslot, userinfo;"
+  
+    db.query(q, (err, data) => {
+      if (data) return res.status(500).json();
+      //res.send('Parkinginfo table created!');
+      return res.status(200).json("Error!");
+    });
+};
+
+async function api_4() {
+  const api4 = (req, res) => {
+      let q = "SELECT email FROM occupiedslot where email = $email"
+    
+      db.query(q, (err, data) => {
+        if (data) return res.status(200).json("User already exists");
+        //res.send('Parkinginfo table created!');
+        return res.status(500).json();
+        //const [result1] = await connection.execute("DELETE FROM occupiedslot WHERE email = $email");
+        //return result1;
+      });
+  };
+
+  const [result1] = await connection.execute("DELETE FROM occupiedslot WHERE email = $email");
+  const [result2] = await connection.execute("UPDATE parkinginfo SET exitTime = NOW(), timediff = TIMEDIFF(MINUTE, exitTime, entryTime), cost = timediff * 0.5 where  email = $email");
+
+}
+
 setInterval(() => db.query("select 1"), 10000);
 
 app.listen(3001, (req, res) => {
