b3faf4d10bdcdc8232e221452584798a7e50a3a0
GOWTHAM BAALAJI K <103375595+gowthamgowthi4@users.noreply.github.com>
2023-04-09 00:37:36 +0530

    Update park.js

--- controllers/park.js
+++ controllers/park.js
@@ -60,13 +60,27 @@ export const park = (req, res) => {
 };
 
 export const unpark = (req, res) => {
-  let q = `SELECT email FROM occupiedslot where email = ${req.params.email}`;
-
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json(err);
-    return res.status(200).json(data);
+  let q1 = `SELECT email FROM occupiedslot where email = ${req.params.email}`
+  let q2 = `DELETE * FROM occupiedslot WHERE email = ${req.params.email}`
+  let q3 = `UPDATE parkinginfo SET exitTime = NOW(), timediff = DATEDIFF(MINUTE, exitTime, entryTime), cost = timediff*0.5 where parkinginfo.email = ${req.params.email}`
+  db.query(q1, (err1, data1) => {
+    if (err1) return res.status(200).json("User not found!");
+    else {
+      db.query(q2, (err2, data2) => {
+        if(err2) return res.status(200).json("Data cannot be deleted!");
+        else {
+          db.query(q3, (err3, data3) => {
+            if(err3) return res.status(200).json("Data cannot be inserted into parkinginfo Table!");
+            else {
+              return res.status(500).json("Success!");
+            }
+          })
+        }
+      })
+    }
   });
 };
+
 
 // export const park = (req, res) => {
 //     const q1 = `INSERT INTO occupiedslot(slotname, floor, numberplate, email, username, phonenumber) SELECT f.slotname, f.floor, u.numberplate, u.email, u.username, u.phonenumber FROM freeslot f, userinfo u where u.email = '${req.params.email}';`;
